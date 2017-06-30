46fe5e54c4a43ed1384556a1af376ae21d814bf3
Matko Raguz <m.raguz@gmail.com>
2017-06-30 15:08:21 +0200

    Removed unused code

--- node/src/controllers/index.controller.ts
+++
@@ -1,5 +0,0 @@
-import {Request, Response} from 'express';
-
-export const getIndex = (req: Request, res: Response) => {
-	res.send('index');
-};
--- node/src/controllers/users.controller.ts
+++ node/src/controllers/users.controller.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from 'express';
 
 export const getAllUsers = (req: Request, res: Response) => {
-	let bla = '5';
-	res.send('blabla5' + bla);
+	res.send('allUsers API route');
 };
